50b2fd87e3c24aadd23daaafcdbdb123be112369
unknown <jacriollo09@utpl.edu.ec>
2021-06-24 15:38:49 -0500

    TrabajoGrupal

---
+++ TrabajoGrupal/src/trabajogrupal/menuEconomico.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogrupal;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class menuEconomico {
+    
+}
