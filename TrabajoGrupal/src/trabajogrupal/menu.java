b51d8f6af5597e995c04bcb8513f28045d7bd228
Jhoel Ordoñez <jhoelordonez2@gmail.com>
2021-06-24 15:44:44 -0500

    TrabajoGrupal

--- TrabajoGrupal/src/trabajogrupal/menu.java
+++ TrabajoGrupal/src/trabajogrupal/menu.java
@@ -10,5 +10,39 @@ package trabajogrupal;
  * @author Usuario iTC
  */
 public class menu {
+    protected String nombrePlato;
+    protected double valorMenu;
+    protected double valorInicial;
+    
+    public menu(String nP, double vM, double vI){
+        nombrePlato = nP;
+        valorMenu = vM;
+        valorInicial = vI;
+    }
+
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+
+    public void setNombrePlato(String nombrePlato) {
+        this.nombrePlato = nombrePlato;
+    }
+
+    public double getValorMenu() {
+        return valorMenu;
+    }
+
+    public void setValorMenu(double valorMenu) {
+        this.valorMenu = valorMenu;
+    }
+
+    public double getValorInicial() {
+        return valorInicial;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+    
     
 }
